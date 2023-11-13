12f92589d7f072c84f33fc9628efec0bd6c69cce
Anna Solovkina <aasolovkina@gmail.com>
2023-11-13 23:54:05 +0300

    Выполнила задачу номер один из 4 модуля

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,15 @@
 function makeFriendsList(friends) {
   // ваш код...
+
+  let friendsList = document.createElement('UL');
+
+  friends.map(function(friend) {
+      return friend.firstName + ' ' + friend.lastName;
+    }).forEach(function(friendName) {
+      let myFriendsListItem = document.createElement('LI');
+      myFriendsListItem.textContent = friendName;
+      friendsList.append(myFriendsListItem);
+    });
+
+  return friendsList;
 }
