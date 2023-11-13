9ee95668fe42ecbdbc6d5cb07561414c4876cc10
Anna Solovkina <aasolovkina@gmail.com>
2023-11-14 01:12:34 +0300

    Выполнила задачу номер три из 4 модуля

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,25 @@
 function highlight(table) {
   // ваш код...
+  for (let tr of table.querySelectorAll('tbody > tr')) {
+    if (!tr.cells[3].hasAttribute('data-available')) {
+      tr.setAttribute('hidden', '');
+    }
+    
+    if (tr.cells[3].getAttribute('data-available') == 'true') {
+      tr.classList.add('available');
+    } else {
+      tr.classList.add('unavailable');
+    }
+
+    if (tr.cells[2].textContent == 'm') {
+      tr.classList.add('male');
+    } else if (tr.cells[2].textContent == 'f') {
+      tr.classList.add('female');
+    }
+
+    let age = +tr.cells[1].textContent;
+    if (!isNaN(age) && age < 18) {
+      tr.style.cssText += 'text-decoration: line-through';
+    }
+  }
 }
