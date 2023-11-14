be10189d8e243bf7f735ec4ce761d269d5e87b38
Anna Solovkina <aasolovkina@gmail.com>
2023-11-14 22:48:29 +0300

    Выполнила задачу номер два из 5 модуля

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,17 @@
 function toggleText() {
   // ваш код...
+
+  let button = document.querySelector('.toggle-text-button');
+  
+  button.addEventListener('click', function(event) {
+    let textElement = document.getElementById('text');
+
+    if (textElement) {
+      if (textElement.hasAttribute('hidden')) {
+        textElement.removeAttribute('hidden');
+      } else {
+        textElement.setAttribute('hidden', '')
+      }
+    }
+  });
 }
