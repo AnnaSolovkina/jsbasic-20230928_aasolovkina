bddeb58e1630ca616bd7e82fb6122810fba324d5
Anna Solovkina <aasolovkina@gmail.com>
2023-11-14 21:00:51 +0300

    Выполнила задачу номер один из 5 модуля

--- 5-module/1-task/index.js
+++ 5-module/1-task/index.js
@@ -1,3 +1,11 @@
 function hideSelf() {
   // ваш код...
+  let button = document.querySelector('.hide-self-button');
+
+  button.addEventListener('click', function(event) {
+    event.currentTarget.setAttribute('hidden', '');
+  });
 }
+
+
+
